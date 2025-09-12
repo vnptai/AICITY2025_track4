2ac855e2f3e47bc0035f4a51a50afc7e34295090
hunglc007 <hunglc007@gmail.com>
2025-09-12 18:33:53 +0700

    First commit

--- scripts/gen_data.sh
+++ scripts/gen_data.sh
@@ -1,3 +1,4 @@
 cd labeling
 cd mmdetection
-python tools/test.py projects/CO-DETR/configs/train_set_gen.py ../../weights/CO_DETR.pth
+#python tools/test.py projects/CO-DETR/configs/train_set_gen.py ../../weights/CO_DETR.pth
+python tools/test.py projects/CO-DETR/configs/val_set_gen.py ../../weights/CO_DETR.pth
